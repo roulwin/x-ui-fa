a442228a12bd4faa615a5ded587d473e357c9071
Telegram: @hcentury <67808603+MrCenTury@users.noreply.github.com>
2022-11-03 12:20:35 +0330

    Update inbound.go

--- web/controller/inbound.go
+++ web/controller/inbound.go
@@ -1,7 +1,11 @@
 package controller
 
 import (
+	"fmt"
+	"math/rand"
 	"strconv"
+	"strings"
+	"time"
 	"x-ui/database/model"
 	"x-ui/logger"
 	"x-ui/web/global"
@@ -16,6 +20,18 @@ type InboundController struct {
 	xrayService    service.XrayService
 }
 
+func randomString(length int) string {
+	rand.Seed(time.Now().UnixNano())
+	var alphabet string = "ABCDEFGHIGKLMNOPQRSTUVWXYZabcdefghigklmnopqrstuvwxyz0123456789"
+	var sb strings.Builder
+	l := len(alphabet)
+	for i := 0; i < length; i++ {
+		c := alphabet[rand.Intn(l)]
+		sb.WriteByte(c)
+	}
+	return sb.String()
+}
+
 func NewInboundController(g *gin.RouterGroup) *InboundController {
 	a := &InboundController{}
 	a.initRouter(g)
@@ -25,13 +41,13 @@ func NewInboundController(g *gin.RouterGroup) *InboundController {
 
 func (a *InboundController) initRouter(g *gin.RouterGroup) {
 	g = g.Group("/inbound")
+
 	g.POST("/list", a.getInbounds)
 	g.POST("/add", a.addInbound)
 	g.POST("/del/:id", a.delInbound)
 	g.POST("/update/:id", a.updateInbound)
 	g.POST("/clientIps/:email", a.getClientIps)
 	g.POST("/clearClientIps/:email", a.clearClientIps)
-
 }
 
 func (a *InboundController) startTask() {
@@ -41,7 +57,7 @@ func (a *InboundController) startTask() {
 		if a.xrayService.IsNeedRestartAndSetFalse() {
 			err := a.xrayService.RestartXray(false)
 			if err != nil {
-				logger.Error(" راه اندازی مجدد ناموفق بود: ", err)
+				logger.Error("restart xray failed:", err)
 			}
 		}
 	})
@@ -51,7 +67,7 @@ func (a *InboundController) getInbounds(c *gin.Context) {
 	user := session.GetLoginUser(c)
 	inbounds, err := a.inboundService.GetInbounds(user.Id)
 	if err != nil {
-		jsonMsg(c, "دریافت", err)
+		jsonMsg(c, "获取", err)
 		return
 	}
 	jsonObj(c, inbounds, nil)
@@ -61,14 +77,15 @@ func (a *InboundController) addInbound(c *gin.Context) {
 	inbound := &model.Inbound{}
 	err := c.ShouldBind(inbound)
 	if err != nil {
-		jsonMsg(c, "ایجاد", err)
+		jsonMsg(c, "添加", err)
 		return
 	}
 	user := session.GetLoginUser(c)
 	inbound.UserId = user.Id
 	inbound.Enable = true
+	inbound.Tag = fmt.Sprintf("inbound-%v", randomString(10))
 	err = a.inboundService.AddInbound(inbound)
-	jsonMsg(c, "ایجاد", err)
+	jsonMsg(c, "添加", err)
 	if err == nil {
 		a.xrayService.SetToNeedRestart()
 	}
@@ -77,11 +94,11 @@ func (a *InboundController) addInbound(c *gin.Context) {
 func (a *InboundController) delInbound(c *gin.Context) {
 	id, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
-		jsonMsg(c, "حذف", err)
+		jsonMsg(c, "删除", err)
 		return
 	}
 	err = a.inboundService.DelInbound(id)
-	jsonMsg(c, "حذف", err)
+	jsonMsg(c, "删除", err)
 	if err == nil {
 		a.xrayService.SetToNeedRestart()
 	}
@@ -90,7 +107,7 @@ func (a *InboundController) delInbound(c *gin.Context) {
 func (a *InboundController) updateInbound(c *gin.Context) {
 	id, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
-		jsonMsg(c, "ویرایش", err)
+		jsonMsg(c, "修改", err)
 		return
 	}
 	inbound := &model.Inbound{
@@ -98,22 +115,21 @@ func (a *InboundController) updateInbound(c *gin.Context) {
 	}
 	err = c.ShouldBind(inbound)
 	if err != nil {
-		jsonMsg(c, "ویرایش", err)
+		jsonMsg(c, "修改", err)
 		return
 	}
 	err = a.inboundService.UpdateInbound(inbound)
-	jsonMsg(c, "ویرایش", err)
+	jsonMsg(c, "修改", err)
 	if err == nil {
 		a.xrayService.SetToNeedRestart()
 	}
 }
-
 func (a *InboundController) getClientIps(c *gin.Context) {
 	email := c.Param("email")
 
 	ips, err := a.inboundService.GetInboundClientIps(email)
 	if err != nil {
-		jsonObj(c, "آی پی جدیدی ثبت نشده است", nil)
+		jsonObj(c, "No IP Record", nil)
 		return
 	}
 	jsonObj(c, ips, nil)
@@ -123,8 +139,8 @@ func (a *InboundController) clearClientIps(c *gin.Context) {
 
 	err := a.inboundService.ClearClientIps(email)
 	if err != nil {
-		jsonMsg(c, "ویرایش", err)
+		jsonMsg(c, "修改", err)
 		return
 	}
-	jsonMsg(c, "پاک کردن گزارش", nil)
+	jsonMsg(c, "Log Cleared", nil)
 }
